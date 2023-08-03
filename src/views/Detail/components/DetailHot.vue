8ff1cc9b7c3462463a51e124b801ae0a371e1299
watch <1677867144@qq.com>
2023-08-03 19:26:00 +0800

    放大镜效果实现

--- src/views/Detail/components/DetailHot.vue
+++ src/views/Detail/components/DetailHot.vue
@@ -1,26 +1,37 @@
 <script setup>
 import { fetchHotGoodsAPI } from '@/apis/detail'
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+//设计props参数，适配title和数据
+const props = defineProps({
+  hotType: Number
+})
+
+
 const hotList = ref([])
 const route = useRoute()
 const gethotList = async () => {
   const res = await fetchHotGoodsAPI({
     id: route.params.id,
-    type: 1,
+    type: props.hotType,
   })
   hotList.value = res.result
 }
 onMounted(() => {
   gethotList()
 })
+const typemap = {
+  1: '24小时热榜',
+  2: '周热榜'
+}
+const title = computed(() => typemap[props.hotType])
 
 </script>
 
 
 <template>
   <div class="goods-hot">
-    <h3>周日榜单</h3>
+    <h3>{{ title }}</h3>
     <!-- 商品区块 -->
     <RouterLink to="/" class="goods-item" v-for="item in hotList" :key="item.id">
       <img :src="item.picture" alt="" />
--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -4,6 +4,7 @@ import { getDetail } from '@/apis/detail'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import DetailHot from './components/DetailHot.vue'
+import ImgView from '@/components/ImgView/index.vue'
 const goodsList = ref({})
 const route = useRoute()
 const getGoods = async () => {
@@ -46,7 +47,7 @@ onMounted(() => {
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-
+              <ImgView />
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
@@ -131,10 +132,9 @@ onMounted(() => {
             <!-- 24热榜+专题推荐 -->
             <div class="goods-aside">
               <!-- 24小时-->
-              <DetailHot />
+              <DetailHot :hotType="1" />
               <!-- 周-->
-
-              <DetailHot />
+              <DetailHot :hotType="2" />
             </div>
           </div>
         </div>
